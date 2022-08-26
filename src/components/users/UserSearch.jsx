9f54a3c1f1e2971b79709d028f21bfe955de82d9
George B <george@wizardofweb.com>
2022-08-26 20:39:01 +0800

    fixed clear button and added search header

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,61 +1,68 @@
-import React from 'react';
-import { useState, useContext } from 'react';
-import GithubContext from '../../context/github/GithubContext';
-import AlertContext from '../../context/alert/AlertContext';
-import { searchUsers } from '../../context/github/GithubActions';
+import React from 'react'
+import { useState, useContext } from 'react'
+import GithubContext from '../../context/github/GithubContext'
+import AlertContext from '../../context/alert/AlertContext'
+import { searchUsers } from '../../context/github/GithubActions'
 
 function UserSearch() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState('')
 
-  const { users, dispatch, clearUsers } = useContext(GithubContext);
-  const { setAlert } = useContext(AlertContext);
+  const { users, dispatch } = useContext(GithubContext)
+  const { setAlert } = useContext(AlertContext)
 
-  const handleChange = (e) => setText(e.target.value);
+  const handleChange = (e) => setText(e.target.value)
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     if (text === '') {
-      setAlert('Please enter something', 'error');
+      setAlert('Please enter something', 'error')
     } else {
-      dispatch({ type: 'SET_LOADING' });
-      const users = await searchUsers(text);
-      dispatch({ type: 'GET_USERS', payload: users });
-      setText('');
+      dispatch({ type: 'SET_LOADING' })
+      const users = await searchUsers(text)
+      dispatch({ type: 'GET_USERS', payload: users })
+      setText('')
     }
-  };
+  }
+
+  const clearUsers = () => {
+    dispatch({ type: 'CLEAR_USERS' })
+  }
 
   return (
-    <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div className='form-control'>
-            <div className='relative'>
-              <input
-                type='text'
-                className='w-full pr-40 bg-gray-200 input input-lg text-black'
-                placeholder='Search'
-                value={text}
-                onChange={handleChange}
-              />
-              <button
-                type='submit'
-                className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'>
-                Go
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-      {users.length > 0 && (
+    <>
+      <h2 className='font-medium text-2xl pb-3'>Search for a github user</h2>
+      <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
         <div>
-          <button onClick={clearUsers} className='btn btn-ghost btn-lg'>
-            Clear
-          </button>
+          <form onSubmit={handleSubmit}>
+            <div className='form-control'>
+              <div className='relative'>
+                <input
+                  type='text'
+                  className='w-full pr-40 bg-gray-200 input input-lg text-black'
+                  placeholder='Search'
+                  value={text}
+                  onChange={handleChange}
+                />
+                <button
+                  type='submit'
+                  className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'>
+                  Go
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
-      )}
-    </div>
-  );
+        {users.length > 0 && (
+          <div className='pt-12.7'>
+            <button onClick={clearUsers} className='btn btn-ghost btn-lg'>
+              Clear
+            </button>
+          </div>
+        )}
+      </div>
+    </>
+  )
 }
 
-export default UserSearch;
+export default UserSearch
